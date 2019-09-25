b14431e8a9833509107889e145a83fcc908f03bc
toshi-oliver <igpoid@gmail.com>
2019-09-25 19:54:22 +0900

    create post.data with db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env =="production"
   (1..25).each do |i|
-    Post.create(client_name: "テスト#{i}", email: "タイトル#{i}", parking_address: "東京都新宿区#{i}丁目")
+    Post.create(client_name: "テスト#{i}", email: "test#{i}@example.com", phone_number: "09049389103", near_station: "中野坂上", parking_address: "東京都新宿区西新宿#{i}丁目")
   #Post.create(client_name: "toshiaki", email: "igpoid@gmail.com", phone_number: "090-4938-9103", near_station: "江古田", parking_address: "東京都新宿区高田馬場")
   end
   
-  User.create(name: "admin", email: "igpoid@ontech.tokyo", password: "password", password_confirmation: "password", admin: true)
-  User.create(name: "古川", email: "furukawa@ontech.tokyo", password: "furukawa0626", password_confirmation: "furukawa0626", admin: false)
-  User.create(name: "中津", email: "nakatsu@ontech.tokyo", password: "nakatsu0626", password_confirmation: "nakatsu0626", admin: false)
-  User.create(name: "渡邉", email: "watanabe@ontech.tokyo", password: "watanabe0626", password_confirmation: "watanabe0626", admin: false)
-  User.create(name: "根間", email: "nema@ontech.tokyo", password: "nema0626", password_confirmation: "nema0626", admin: false)
-  User.create(name: "堀上", email: "horikami@ontech.tokyo", password: "horikami0626", password_confirmation: "horikami0626", admin: false)
+  #User.create(name: "admin", email: "igpoid@ontech.tokyo", password: "password", password_confirmation: "password", admin: true)
+  #User.create(name: "古川", email: "furukawa@ontech.tokyo", password: "furukawa0626", password_confirmation: "furukawa0626", admin: false)
+  #User.create(name: "中津", email: "nakatsu@ontech.tokyo", password: "nakatsu0626", password_confirmation: "nakatsu0626", admin: false)
+  #User.create(name: "渡邉", email: "watanabe@ontech.tokyo", password: "watanabe0626", password_confirmation: "watanabe0626", admin: false)
+  #User.create(name: "根間", email: "nema@ontech.tokyo", password: "nema0626", password_confirmation: "nema0626", admin: false)
+  #User.create(name: "堀上", email: "horikami@ontech.tokyo", password: "horikami0626", password_confirmation: "horikami0626", admin: false)
 end
